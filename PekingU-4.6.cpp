70979fb292b8275510b2384cf3b33e30462215d0
Rin_Ray <32867034+jobsk@users.noreply.github.com>
2020-10-05 21:08:47 +0800

    Update PekingU-4.6.cpp
     error: no match for ‘operator>>’ (operand types are ‘std::istream {aka std::basic_istream}’ and ‘Complex’)
         std::cin >> c >> n;
     error: no match for ‘operator<<’ (operand types are ‘std::ostream {aka std::basic_ostream}’ and ‘Complex’)
         std::cout << c <<", " << n;

--- PekingU-4.6.cpp
+++ PekingU-4.6.cpp
@@ -3,27 +3,29 @@
 #include <cstdlib>
 
 using std::string;
+using std::istream;
+using std::ostream;
 
 class Complex
 {
     double real, imag;
     public:
         Complex(double r=0, double i=0):real(r), imag(i){};
-        ostream& operator <<(ostream& os, const Complex& c);
-        istream& operator >>(istream& is, Complex& c);
+        ostream& operator <<(ostream& os);
+        istream& operator >>(istream& is);
 };
 
 ostream& Complex::operator <<(ostream& os)
 {
     os << this->real << "+" << this->imag << "i";
-    retrun os;
+    return os;
 }
 
 istream& Complex::operator >>(istream& is)
 {
     string s;
     is >> s; // 将 a+bi 作为字符串读入，a+bi 是一个整体
-    int pos = find("+", 0); // pos : The initial position from where the string search is to begin.
+    int pos = s.find("+", 0); // pos : The initial position from where the string search is to begin.
                              // size_t find (const char* s, size_t pos = 0); 
                              // s : The sub-string to be searched, given as C style string.
     string sTmp = s.substr(0, pos); // 从 s[0]取到s[pos] 因为 参数2 len是 pos-0
@@ -31,6 +33,15 @@ istream& Complex::operator >>(istream& is)
     // The c_str() function is used to return a pointer to an array that contains a null terminated sequence of character representing the current value of the string.
     
     sTmp = s.substr(pos+1, s.length()-pos-2); // 多减掉一个i的长度
-    imag = atof(sTmp.c_str);
-    retrun is;
+    imag = atof(sTmp.c_str());
+    return is;
+}
+
+int main()
+{
+    Complex c;
+    int n;
+    std::cin >> c >> n;
+    std::cout << c <<", " << n;
+    return 0;
 }
